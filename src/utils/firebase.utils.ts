372074e63b261209aba63d80dabc445273535926
Muftahuddeen Abba <muftahuddeenabba@gmail.com>
2023-06-04 23:04:28 +0100

    blank slate

---
+++ src/utils/firebase.utils.ts
@@ -0,0 +1,54 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
+import "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCF2zSgYCMVG3OvsjBN6pxumRir_TOhfFA",
+  authDomain: "bookish-winner.firebaseapp.com",
+  projectId: "bookish-winner",
+  storageBucket: "bookish-winner.appspot.com",
+  messagingSenderId: "930937650548",
+  appId: "1:930937650548:web:c1c0775d4ff22220078021",
+  measurementId: "G-MWDWDRMWX7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+      console.log(`Access token: ${token}`);
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      console.log("user:", user);
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
+
+// const analytics = getAnalytics(app);
