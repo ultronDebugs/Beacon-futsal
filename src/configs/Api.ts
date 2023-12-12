aca5f9cadb53f58948503ae8159c5f2a9d2c37a4
Muftahuddeen Abba <muftahuddeenabba@gmail.com>
2023-12-12 15:58:33 +0100

    backend updated for testing

--- src/configs/Api.ts
+++ src/configs/Api.ts
@@ -1,3 +1,3 @@
-export const backendApi = "http://localhost:3000/api";
-// export const backendApi = "https://footytimeapi.onrender.com/api";
+// export const backendApi = "http://localhost:3000/api";
+export const backendApi = "https://footytimeapi.onrender.com/api";
 // backend api url
